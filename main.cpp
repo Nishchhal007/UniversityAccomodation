038c61f926702ecf48d16eeb2d0133edc19ae451
Nishchhal <nishchhal901@gmail.com>
2023-01-10 15:44:33 +0530

    Title Added

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+void title();
+
+int main()
+{
+    title();
+
+    return 0;
+}
+
+void title()
+{
+    printf("\n\n\n");
+    printf("\t\t\t\t\t");
+    printf("\n");
+    printf("\t\t\t\t    ---------------------------------\n");
+    printf("\t\t\t\t    |University Accomodation Handling|\n");
+    printf("\t\t\t\t    ---------------------------------\n");
+    printf("\n\n");
+    printf("\t\t\t\t");
+}
